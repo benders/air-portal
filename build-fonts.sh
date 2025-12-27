9fb67ffa16fa025e519cf88883542f71e6778a35
Nic Benders <nic.benders@gmail.com>
2025-12-27 10:36:56 -0800

    Test for utils

--- build-fonts.sh
+++ build-fonts.sh
@@ -1,8 +1,19 @@
-#!/bin/bash -ex
+#!/bin/bash -e
 
+# Check if required utilities are available
+for cmd in otf2bdf bdftopcf; do
+    if ! command -v "$cmd" &> /dev/null; then
+        echo "Error: Required utility '$cmd' not found in PATH or not executable"
+        echo "On macOS, install with: brew install otf2bdf bdftopcf"
+        exit 1
+    fi
+done
+
+# DPI set to 72 for compatibility with CircuitPython Fonts Bundle 
 DISPLAY_DPI=72
-
 FONT=Federation
+
+set -x
 rm -f fonts/${FONT}-*-latin1.bdf fonts/${FONT}-*-latin1.pcf
 for size in 20 96; do
     otf2bdf -r ${DISPLAY_DPI} -l 32_255 -p ${size} "fonts-src/${FONT}.ttf" -o "fonts/${FONT}-${size}-latin1.bdf"
